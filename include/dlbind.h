a59ae1e21ada6c4a41ef0be4e8f20504511dfe11
Stephen Kell <srk31@cl.cam.ac.uk>
2016-10-31 15:06:52 +0000

    Minor cleanup, and move header to include/.

--- include/dlbind.h
+++ include/dlbind.h
@@ -6,11 +6,8 @@
 /* We extend the libdl interface with calls to dynamically create 
  * new libraries. */
 
-//struct Ld_Entry;
-//typedef struct Ld_Entry *ld_handle_t;
-
 /* Create a new shared library in this address space. */
-void * dlcreate(const char *libname);
+void *dlcreate(const char *libname);
 
 void *dlreload(void *handle);
 
@@ -33,22 +30,22 @@ int dldelete(void *);
 void *dldsym(void *handle, const char *symbol, void *namespace,
     unsigned long conv, const char *version_string);
 
-extern const char *greatest_version;
-
 /* The "conv" descriptor has the following encoding. 
  * LSB: must be 1 -- allows us to extend the encoding 
         to be specified by a pointer to block, later.
  * 4 bits: describing per-call options
  * 3 bits * n: describing per-argument options, for up to 
         9 arguments (32-bit sytems), or
-        19 arguments (64-bit systems). */
+        19 arguments (64-bit systems).
+ * 
+ * Think "multiple entry points".
+ */
 enum argument_options
 {
 	INDIRECT = 1,
 	APP_CHECKED = 2,   /* e.g. nonnull, ... */
-	LANGVM_CHECKED = 4, /* e.g. already a valid Java object, ... 
-	                       -- hmm, could merge this with INDIRECT and save bits? 
-	                          i.e. */
+	LANGVM_CHECKED = 4, /* e.g. already [known to be] a valid Java object, ... 
+	                       -- hmm, could merge this with INDIRECT and save bits? */
 	MAX_PLUS_ONE = 8
 };
 
