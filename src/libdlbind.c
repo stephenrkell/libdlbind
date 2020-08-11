7e3f081356d1a7236ef3f73279fb11d58ceb9425
Stephen Kell <S.R.Kell@kent.ac.uk>
2020-08-11 19:19:23 +0100

    Improve error handling, and do tiny cleanups

--- src/libdlbind.c
+++ src/libdlbind.c
@@ -12,6 +12,7 @@
 #include <link.h>
 #include <unistd.h>
 #include <sys/statvfs.h>
+#include <err.h>
 #include "elfproto.h"
 #include "symhash.h"
 #include "dlbind.h"
@@ -40,21 +41,17 @@ void *dlalloc(void *handle, size_t sz, unsigned flags)
 			// use .text:
 			dt_bump = DT_DLBIND_TEXTBUMP;
 			break;
-		
 		case (SHF_WRITE|SHF_EXECINSTR):
-			// not allowed -- use 
+			// not allowed -- use multiple mappings
 			return NULL;
-		
 		case 0:
 			// use .rodata:
 			dt_bump = DT_DLBIND_RODATABUMP;
 			break;
-		
 		default:
 			return NULL;
 	}
-	
-	
+
 	for (Elf64_Dyn *d = (Elf64_Dyn*) l->l_ld; d->d_tag != DT_NULL; ++d)
 	{
 		if (d->d_tag == dt_bump)
@@ -64,7 +61,7 @@ void *dlalloc(void *handle, size_t sz, unsigned flags)
 			return (void*)((uintptr_t) l->l_addr + ret);
 		}
 	}
-	
+
 	return NULL;
 }
 
@@ -96,9 +93,8 @@ void *dlbind(void *lib, const char *symname, void *obj, size_t len, ElfW(Word) t
 	unsigned strind = dynstr_bump_ent->d_un.d_val;
 	char *dynstr_insertion_pt = (char*) FIXUP_PTR(found_dynstr_ent->d_un.d_ptr, l->l_addr) + strind;
 	unsigned symind = dynsym_bump_ent->d_un.d_val;
-	Elf64_Sym *dynsym_insertion_pt = (Elf64_Sym *) FIXUP_PTR(found_dynsym_ent->d_un.d_ptr, l->l_addr) 
+	Elf64_Sym *dynsym_insertion_pt = (Elf64_Sym *) FIXUP_PTR(found_dynsym_ent->d_un.d_ptr, l->l_addr)
 			+ symind;
-	
 	strcpy(dynstr_insertion_pt, symname);
 	dynstr_bump_ent->d_un.d_val += strlen(symname) + 1;
 
@@ -115,9 +111,7 @@ void *dlbind(void *lib, const char *symname, void *obj, size_t len, ElfW(Word) t
 	Elf64_Shdr *shdr = (Elf64_Shdr*) ((char*) l->l_addr + shdr_sym->st_value);
 	assert(shdr == dlsym(lib, "_SHDRS"));
 	if (!shdr) return NULL;
-	
 	unsigned n_shdrs = shdr_sym->st_size / sizeof (Elf64_Shdr);
-	
 	Elf64_Shdr *preceding_shdr = NULL;
 	for (Elf64_Shdr *i_shdr = shdr; i_shdr < shdr + n_shdrs; ++i_shdr)
 	{
@@ -128,9 +122,8 @@ void *dlbind(void *lib, const char *symname, void *obj, size_t len, ElfW(Word) t
 			preceding_shdr = i_shdr;
 		}
 	}
-	
-	*dynsym_insertion_pt = (Elf64_Sym) { 
-		.st_name = strind, 
+	*dynsym_insertion_pt = (Elf64_Sym) {
+		.st_name = strind,
 		.st_info = ELF64_ST_INFO(STB_GLOBAL, type),
 		.st_other = ELF64_ST_VISIBILITY(STV_DEFAULT),
 		.st_shndx = preceding_shdr ? (preceding_shdr - shdr) : SHN_ABS,
@@ -138,7 +131,6 @@ void *dlbind(void *lib, const char *symname, void *obj, size_t len, ElfW(Word) t
 		.st_size = len
 	};
 	dynsym_bump_ent->d_un.d_val--;
-	
 	elf64_hash_put(
 		(char*) FIXUP_PTR(found_hash_ent->d_un.d_ptr, l->l_addr),    /* hash section */
 		(2 + NBUCKET + MAX_SYMS) * sizeof (Elf64_Word), /* hash section size in bytes */
@@ -155,13 +147,13 @@ void *dlbind(void *lib, const char *symname, void *obj, size_t len, ElfW(Word) t
 // 	if (!found_dynsym_ent) return -3;
 // 	Elf64_Dyn *found_hash_ent = dynamic_lookup(dynamic, DT_HASH);
 // 	if (!found_hash_ent) return -4;
-// 	
+//
 // 	/* Walk the shdrs until we find the dynsym */
 // 	while (shdr->sh_vaddr != found_dynsym_ent->d_un.d_ptr) ++shdr;
 // 	// FIXME: check for end somehow -- maybe _SHDRS symbol size info
-// 	
+//
 // 	unsigned nlocal = shdr->sh_info;
-	
+
 	return dlreload(l);
 }
 
@@ -172,10 +164,10 @@ void *dlreload(void *h)
 	struct link_map *l = (struct link_map *) h;
 	void *old_load_addr = (void*) l->l_addr;
 	char *copied = strdup(l->l_name);
-	
-	/* Un-relocate any relocations applied by ld.so. 
-	 * We didn't create any relocation records, so we are mostly okay. 
-	 * BUT ld.so also relocates certain d_ptr values in the .dynamic section. 
+
+	/* Un-relocate any relocations applied by ld.so.
+	 * We didn't create any relocation records, so we are mostly okay.
+	 * BUT ld.so also relocates certain d_ptr values in the .dynamic section.
 	 * So undo that bit. */
 #ifndef FAKE_RELOAD
 	Elf64_Dyn *found_dynsym_ent = dynamic_lookup(l->l_ld, DT_SYMTAB);
@@ -186,17 +178,17 @@ void *dlreload(void *h)
 	Elf64_Dyn *found_dynstr_ent = dynamic_lookup(l->l_ld, DT_STRTAB);
 	if ((char*) found_dynstr_ent->d_un.d_ptr >= (char*) l->l_addr)
 	{
-		found_dynstr_ent->d_un.d_ptr -= (uintptr_t) l->l_addr; 
+		found_dynstr_ent->d_un.d_ptr -= (uintptr_t) l->l_addr;
 	}
 	Elf64_Dyn *found_hash_ent = dynamic_lookup(l->l_ld, DT_HASH);
 	if ((char*) found_hash_ent->d_un.d_ptr >= (char*) l->l_addr)
 	{
-		found_hash_ent->d_un.d_ptr -= (uintptr_t) l->l_addr; 
+		found_hash_ent->d_un.d_ptr -= (uintptr_t) l->l_addr;
 	}
 	Elf64_Dyn *found_rela_ent = dynamic_lookup(l->l_ld, DT_RELA);
 	if ((char*) found_rela_ent->d_un.d_ptr >= (char*) l->l_addr)
 	{
-		found_rela_ent->d_un.d_ptr -= (uintptr_t) l->l_addr; 
+		found_rela_ent->d_un.d_ptr -= (uintptr_t) l->l_addr;
 	}
 	int failed = dlclose(l);
 	_Bool still_loaded = 0;
@@ -250,7 +242,6 @@ void *dlreload(void *h)
 	if (old_handle->l_prev) old_handle->l_prev->l_next = old_handle->l_next;
 	if (old_handle->l_next) old_handle->l_next->l_prev = old_handle->l_prev;
 	r_brk();
-	
 	/* Now put it back and signal again. HACK again. */
 	if (old_handle->l_prev) old_handle->l_prev->l_next = old_handle;
 	if (old_handle->l_next) old_handle->l_next->l_prev = old_handle;
@@ -266,13 +257,11 @@ static _Bool path_is_viable(const char *path)
 {
 	_Bool can_access = (0 == access(path, R_OK|W_OK|X_OK));
 	if (!can_access) return 0;
-	
 	struct statvfs buf;
 	int ret = statvfs(path, &buf);
 	if (ret != 0) return 0;
 	if (buf.f_flag & ST_RDONLY) return 0;
 	if (buf.f_flag & ST_NOEXEC) return 0;
-	
 	return 1;
 }
 
@@ -290,14 +279,14 @@ void *dlcreate(const char *libname)
 	char filename0[] = "/run/shm/tmp.dlbind.XXXXXX";
 	char filename1[] = "/tmp/tmp.dlbind.XXXXXX";
 	char *filename = path_is_viable("/run/shm/.") ? filename0 : filename1;
-	int fd = mkostemp(&filename[0], O_RDWR|O_CREAT);
-	assert(fd != -1);
+	int fd = mkostemp(filename, O_RDWR|O_CREAT);
+	if (fd == -1) err(1, "mkostemp(\"%s\", O_RDWR|O_CREAT)", filename);
 	unlink_list[next_free_unlink_entry++] = strdup(filename);
 	/* Truncate the file to the necessary size */
 	int ret = ftruncate(fd, _dlbind_elfproto_memsz);
-	assert(ret == 0);
+	if (ret != 0) err(1, "truncating %s to %ld bytes", filename, _dlbind_elfproto_memsz);
 	void *addr = mmap(NULL, _dlbind_elfproto_memsz, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-	assert(addr != MAP_FAILED);
+	if (addr == MAP_FAILED) err(1, "mapping %s", filename);
 	/* Copy in the ELF proto contents. We don't need to copy the actual sections
 	 * area, which should all be zero. And be even more clever about sparseness,
 	 * since large parts of the  dynsym and dynstr are initially zeroed too. */
@@ -308,12 +297,26 @@ void *dlcreate(const char *libname)
 	dlbind_open_active_on = filename;
 	struct link_map *handle = dlopen(filename, RTLD_NOW | RTLD_GLOBAL/* | RTLD_NODELETE*/);
 	dlbind_open_active_on = NULL;
-	assert(handle);
-
 	/* We can't unlink the file because we need it to be visible in the
 	 * fs namespace, e.g. for debuggers. However, we want to unlink it
-	 * when the program exits or when it is dlclose()d. HACK / FIXME:
-	 * for now we just use a destructor to unlink it when the program exits. */
+	 * when the program exits or when it is dlclose()d.
+	 * HACK / FIXME: for now we just use a destructor to unlink it when
+	 * the program exits (see unlink_all() below).
+	 *
+	 * If it weren't for debuggers, ideally we would now:
+	 * - unlink the filename
+	 * - keep the fd
+	 * - when we dlreopen, do so using the fd.
+	 * This is probably one for when runtld/libgerald are more developed,
+	 * so that we can rely on having a dlopen_from_fd call.
+	 *
+	 * BUT to solve the debugger problem we really want to keep the file
+	 * addressable but not in a way that persists. We can set the l_name to
+	 * any filename we like. Some variation on /proc/pid/core would be enough,
+	 * but alas I don't know of anything that would work.
+	 */
+	if (!handle) err(1, "dlopening %s", filename);
+
 
 	return handle;
 }
